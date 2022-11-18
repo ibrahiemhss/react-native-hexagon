7ba6d1065249354166adb07d61537f2bb3f5fe00
ibrahimhss <ibrhimhssss@gmail.com>
2022-11-18 20:32:16 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-hexagon
+
+hexagon view
+
+## Installation
+
+```sh
+npm install react-native-hexagon
+```
+
+## Usage
+
+```js
+import { HexagonView } from "react-native-hexagon";
+
+// ...
+
+<HexagonView color="tomato" />
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import {
+  requireNativeComponent,
+  UIManager,
+  Platform,
+  ViewStyle,
+} from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-hexagon' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+type HexagonProps = {
+  color: string;
+  style: ViewStyle;
+};
+
+const ComponentName = 'HexagonView';
+
+export const HexagonView =
+  UIManager.getViewManagerConfig(ComponentName) != null
+    ? requireNativeComponent<HexagonProps>(ComponentName)
+    : () => {
+        throw new Error(LINKING_ERROR);
+      };
