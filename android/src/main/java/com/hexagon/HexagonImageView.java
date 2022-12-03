43f504f65b17f8b2ed447197d51bd2fb23a13d65
bob <bob@>
2022-12-04 00:59:13 +0200

    update

--- android/src/main/java/com/hexagon/HexagonImageView.java
+++ android/src/main/java/com/hexagon/HexagonImageView.java
@@ -24,6 +24,7 @@ import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
+import android.media.ThumbnailUtils;
 import android.util.AttributeSet;
 
 import androidx.annotation.ColorInt;
@@ -45,7 +46,6 @@ public class HexagonImageView extends AppCompatImageView {
     private int mSideCount=6;
     private int  mCornerRadius=0;
 
-    float mCenterX, mCenterY=0;
     public HexagonImageView(Context context) {
         super(context);
         setup();
@@ -136,11 +136,23 @@ public class HexagonImageView extends AppCompatImageView {
         BitmapDrawable bitmapDrawable = (BitmapDrawable) this.getDrawable();
 
         if (bitmapDrawable != null){
-            image = bitmapDrawable.getBitmap();
+
+          image = bitmapDrawable.getBitmap();
+
         }
 
     }
-
+  public  Bitmap cropCenter(Bitmap bmp) {
+    int dimension = Math.min(bmp.getWidth(), bmp.getHeight());
+    return ThumbnailUtils.extractThumbnail(bmp, dimension, dimension);
+  }
+  public  Bitmap scaleBitmapAndKeepRation(Bitmap targetBmp,int reqHeightInPixels,int reqWidthInPixels)
+  {
+    Matrix matrix = new Matrix();
+    matrix .setRectToRect(new RectF(0, 0, targetBmp.getWidth(), targetBmp.getHeight()), new RectF(0, 0, reqWidthInPixels, reqHeightInPixels), Matrix.ScaleToFit.CENTER);
+    Bitmap scaledBitmap = Bitmap.createBitmap(targetBmp, 0, 0, targetBmp.getWidth(), targetBmp.getHeight(), matrix, true);
+    return scaledBitmap;
+  }
     @SuppressLint("DrawAllocation")
     @Override
     public void onDraw(Canvas canvas){
@@ -151,18 +163,54 @@ public class HexagonImageView extends AppCompatImageView {
         // init shader
         if (image != null) {
 
+
             canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
-            shader = new BitmapShader(Bitmap.createScaledBitmap(image, canvas.getWidth(), canvas.getHeight(), false), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            shader = new BitmapShader(Bitmap.createScaledBitmap(image, viewWidth, viewHeight, true), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
             paint.setShader(shader);
             canvas.drawPath(hexagonBorderPath, paintBorder);
             canvas.drawPath(hexagonPath, paint);
             canvas.clipPath(hexagonPath, Region.Op.DIFFERENCE);
 
+          //image  =    scaleCenterCrop(image, viewHeight,viewWidth);
+            //canvas.drawBitmap(image, startX, startY, paint);
+           // canvas.drawBitmap(image, (canvas.getWidth()/2-(image.getWidth()/2)), (float) (canvas.getHeight() * .25), paint);
+
 
         }
 
     }
+  public Bitmap scaleCenterCrop(Bitmap source, int newHeight, int newWidth) {
+    int sourceWidth = source.getWidth();
+    int sourceHeight = source.getHeight();
 
+    // Compute the scaling factors to fit the new height and width, respectively.
+    // To cover the final image, the final scaling will be the bigger
+    // of these two.
+    float xScale = (float) newWidth / sourceWidth;
+    float yScale = (float) newHeight / sourceHeight;
+    float scale = Math.max(xScale, yScale);
+
+    // Now get the size of the source bitmap when scaled
+    float scaledWidth = scale * sourceWidth;
+    float scaledHeight = scale * sourceHeight;
+
+    // Let's find out the upper left coordinates if the scaled bitmap
+    // should be centered in the new size give by the parameters
+    float left = (newWidth - scaledWidth) / 2;
+    float top = (newHeight - scaledHeight) / 2;
+
+    // The target rectangle for the new, scaled version of the source bitmap will now
+    // be
+    RectF targetRect = new RectF(left, top, left + scaledWidth, top + scaledHeight);
+
+    // Finally, we create a new bitmap of the specified size and draw our new,
+    // scaled bitmap onto it.
+    Bitmap dest = Bitmap.createBitmap(newWidth, newHeight, source.getConfig());
+    Canvas canvas = new Canvas(dest);
+    canvas.drawBitmap(source, null, targetRect, null);
+
+    return dest;
+  }
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
